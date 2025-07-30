d3cb71e202aa69b60fb5c58543618b5fe55e86e9
nicveggio01 <nic.veggian@gmail.com>
2025-07-30 16:57:04 +0200

    update

--- seconda-app/src/App.jsx
+++ seconda-app/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 import UserCrud from './user-crud-analyst/UserCrud'
 
 function App() {
@@ -10,7 +8,7 @@ function App() {
     <>
       <UserCrud></UserCrud>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- seconda-app/src/user-crud-analyst/UserCrud.jsx
+++ seconda-app/src/user-crud-analyst/UserCrud.jsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
 
+
+const API_URL = "/api/users";
 const UserCrud =()=>{
-    return (<div>
-        <h1> USER CRUD </h1>
+    const [users,setUsers] = useState([]);
 
-    </div>)
+    const getUsers =async ()=>{
+        const response = await fetch(API_URL);
+        const result = await response.json();
+        setUsers(result);
+    }  
+    useEffect(()=>{
+        getUsers()
+    },[])
+
+    return (
+        <div className="container my-5">
+        <h1 className = "mb-4"> USER CRUD </h1>
+
+        <div className="table-responsive">
+            <table className="table table-hover table-bordered align-middle">
+                <thead className ="table-light">
+                    <th>Nome</th>
+                    <th>Username</th>
+                    <th>Telefono</th>
+                    <th>Email</th>
+                </thead>
+                <tbody>
+                    {
+                        users.map((u)=>{
+                            return (
+                                <tr>
+                                    <td>{u.name}</td>
+                                    <td>{u.username}</td>
+                                    <td>{u.phone}</td>
+                                    <td>{u.email}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    )
 }
 
 export default UserCrud;
